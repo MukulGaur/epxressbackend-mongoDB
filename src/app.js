a801037f3ce4d0c88cde200c4a348eda271a7f36
mukul <mukulgaur91@gmail.com>
2022-04-07 22:51:29 +0530

    added templates, views and partials

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+import hbs from 'hbs';
 const port = process.env.PORT || 8000;
 import './db/conn.js';
 import { fileURLToPath } from 'url';
@@ -10,9 +11,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 app.use(express.static(staticPath));
 app.set("view engine", 'hbs');
+app.set('views', templatePath);
+hbs.registerPartials(partialsPath);
 
 app.get('/', (req, res) => {
     res.render("index");
