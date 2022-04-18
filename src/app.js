afa95112da58538f35a5bd356a4707da2df485b8
mukul <mukulgaur91@gmail.com>
2022-04-18 18:50:50 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import './db/conn.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
+import './db/conn.js';
+import Register from './models/registers.js';
+import {json} from 'express';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +16,9 @@ const __dirname = dirname(__filename);
 const staticPath = path.join(__dirname, '../public');
 const templatePath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(staticPath));
 app.set("view engine", 'hbs');
@@ -29,6 +35,36 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.render("register");
+});
+
+// create a new user
+app.post('/register', async (req, res) => {
+    try{
+        
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
+
+        if(password===cpassword){
+            const registerEmployee = new Register({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                gender: req.body.gender,
+                mobile: req.body.mobile,
+                age: req.body.age,
+                password: password,
+                confirmpassword: cpassword
+            });
+
+            const registered = await registerEmployee.save();
+            res.status(201).render("index");
+        }else{
+            res.send("password are not matching");
+        }
+
+    }catch(error){
+        res.send(400).send(error);
+    }
 });
 
 app.listen(port, () => {
